f105883b953553eec429e1424af31588e576276c
Tadayoshi Sato <tadayosi@gmail.com>
2015-04-01 13:45:08 +0900

    Added CBR sample

---
+++ basic/src/test/scala/com/redhat/samples/camel/CBRTest.scala
@@ -0,0 +1,81 @@
+package com.redhat.samples.camel
+
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.test.junit4.CamelTestSupport
+import org.apache.camel.component.mock.MockEndpoint
+import org.hamcrest.Matchers._
+import org.junit.Assert._
+import org.junit.Test
+import org.apache.camel.builder.xml.Namespaces
+
+class CBRTest extends CamelTestSupport {
+
+  override def createRouteBuilder = new RouteBuilder {
+    override def configure {
+      val ns = new Namespaces("soap", "http://schemas.xmlsoap.org/soap/envelope/")
+        .add("ns", "urn:samples-camel:basic:1.0")
+      // format: OFF
+      from("direct:in")
+        .choice
+          .when.xpath("/soap:Envelope/soap:Body/ns:aaa", ns)
+            .to("mock:a")
+          .when.xpath("/soap:Envelope/soap:Body/ns:bbb", ns)
+            .to("mock:b")
+          .otherwise
+            .to("mock:c")
+      // format: ON
+    }
+  }
+
+  private def test(uri: String, xml: String) = {
+    val e = getMockEndpoint(uri)
+    e.expectedMessageCount(1)
+    e.expectedBodiesReceived(xml)
+
+    template.sendBody("direct:in", xml)
+    e.assertIsSatisfied()
+  }
+
+  @Test(timeout = 1000)
+  def a: Unit = {
+    val xml =
+      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="urn:samples-camel:basic:1.0">
+        <soap:Header/>
+        <soap:Body>
+          <ns:aaa>
+            <value>Test</value>
+          </ns:aaa>
+        </soap:Body>
+      </soap:Envelope>
+    test("mock:a", xml.toString)
+  }
+
+  @Test(timeout = 1000)
+  def b: Unit = {
+    val xml =
+      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="urn:samples-camel:basic:1.0">
+        <soap:Header/>
+        <soap:Body>
+          <ns:bbb>
+            <value>Test</value>
+          </ns:bbb>
+        </soap:Body>
+      </soap:Envelope>
+    test("mock:b", xml.toString)
+  }
+
+  @Test(timeout = 1000)
+  def c: Unit = {
+    val xml =
+      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="urn:samples-camel:basic:1.0">
+        <soap:Header/>
+        <soap:Body>
+          <ns:ccc>
+            <value>Test</value>
+          </ns:ccc>
+        </soap:Body>
+      </soap:Envelope>
+    test("mock:c", xml.toString)
+  }
+
+}
