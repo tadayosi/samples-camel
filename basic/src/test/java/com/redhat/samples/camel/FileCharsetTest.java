733d0d9231a0a4251267bb187625b1bfe4425dc6
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-08-08 16:25:54 +0900

    Add sftp example

--- basic/src/test/java/com/redhat/samples/camel/FileCharsetTest.java
+++ basic/src/test/java/com/redhat/samples/camel/FileCharsetTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.redhat.samples.camel.helpers.FTPServer;
+import com.redhat.samples.camel.helpers.SSHServer;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.ftpserver.ftplet.FtpException;
@@ -30,7 +31,13 @@ public class FileCharsetTest extends CamelTestSupport {
                     .log("body = ${body}")
                     .to("file:/tmp/sample/out?charset=EUC-JP");
 
-                from("ftp://sample:password@localhost:2121/in?delete=true&charset=Shift_JIS")
+                from("ftp://sample:password@localhost:2121/in/?delete=true&charset=Shift_JIS")
+                    .log("body = ${body}")
+                    .transform().simple("<<< ${body} >>>")
+                    .log("body = ${body}")
+                    .to("file:/tmp/sample/out?charset=EUC-JP");
+
+                from("sftp://sample@localhost:2222/in?password=password&delete=true&charset=Shift_JIS")
                     .log("body = ${body}")
                     .transform().simple("<<< ${body} >>>")
                     .log("body = ${body}")
@@ -45,23 +52,31 @@ public class FileCharsetTest extends CamelTestSupport {
     private Path inFileFtp = Paths.get("target/ftp/in/hello-ftp.txt");
     private Path outFileFtp = Paths.get("/tmp/sample/out/hello-ftp.txt");
 
+    private Path inFileSftp = Paths.get("target/sftp/in/hello-sftp.txt");
+    private Path outFileSftp = Paths.get("/tmp/sample/out/hello-sftp.txt");
+
     private static FTPServer ftpServer;
+    private static SSHServer sshServer;
 
     @Before
     public void deleteOutFile() throws IOException {
         Files.deleteIfExists(outFile);
         Files.deleteIfExists(outFileFtp);
+        Files.deleteIfExists(outFileSftp);
     }
 
     @BeforeClass
-    public static void startFtpServer() throws FtpException {
+    public static void startServers() throws Exception {
         ftpServer = new FTPServer(2121).start();
+        sshServer = new SSHServer(2222).start();
     }
 
     @AfterClass
-    public static void stopFtpServer() {
+    public static void stopServers() throws Exception {
         ftpServer.stop();
         ftpServer = null;
+        sshServer.stop();
+        sshServer = null;
     }
 
     @Test
@@ -89,6 +104,20 @@ public class FileCharsetTest extends CamelTestSupport {
             String.join(
                 "\n",
                 Files.readAllLines(outFileFtp, Charset.forName("EUC-JP"))),
+            is("<<< こんにちは >>>"));
+    }
+
+    @Test
+    public void helloSftp() throws Exception {
+        Files.write(inFileSftp, "こんにちは".getBytes("Shift_JIS"));
+
+        Thread.sleep(3000);
+
+        assertThat(Files.exists(outFileSftp), is(true));
+        assertThat(
+            String.join(
+                "\n",
+                Files.readAllLines(outFileSftp, Charset.forName("EUC-JP"))),
             is("<<< こんにちは >>>"));
     }
 
---
+++ basic/src/test/java/com/redhat/samples/camel/helpers/SSHServer.java
@@ -0,0 +1,58 @@
+package com.redhat.samples.camel.helpers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+
+public class SSHServer {
+
+    private static final String USERNAME = "sample";
+    private static final String PASSWORD = "password";
+    private static final Path DIR_HOME = Paths.get("target/sftp").toAbsolutePath();
+    private static final Path DIR_IN = Paths.get("target/sftp/in").toAbsolutePath();
+
+    private final SshServer sshServer;
+
+    public SSHServer(int port) throws IOException {
+        sshServer = createServer(port);
+    }
+
+    private SshServer createServer(int port) throws IOException {
+        Files.createDirectories(DIR_IN);
+
+        SshServer sshServer = SshServer.setUpDefaultServer();
+        sshServer.setPort(port);
+        sshServer.setKeyPairProvider(
+            new SimpleGeneratorHostKeyProvider(Paths.get("target/hostkey.ser")));
+        sshServer.setSubsystemFactories(Arrays.asList(
+            new SftpSubsystemFactory()
+        ));
+        sshServer.setPasswordAuthenticator((username, password, session) -> {
+            if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+                sshServer.setFileSystemFactory(
+                    new VirtualFileSystemFactory(DIR_HOME));
+                return true;
+            }
+            return false;
+        });
+        return sshServer;
+    }
+
+    public SSHServer start() throws IOException {
+        sshServer.start();
+        return this;
+    }
+
+    public SSHServer stop() throws IOException {
+        sshServer.stop();
+        return this;
+    }
+
+}
