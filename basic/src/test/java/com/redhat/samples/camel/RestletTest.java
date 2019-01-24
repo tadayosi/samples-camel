12362185943e48ee3c28201a3150b6a60b2c4003
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2019-01-24 17:55:05 +0900

    Add Restlet sample

---
+++ basic/src/test/java/com/redhat/samples/camel/RestletTest.java
@@ -0,0 +1,39 @@
+package com.redhat.samples.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.common.HttpMethods;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class RestletTest extends CamelTestSupport {
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("direct:hello")
+                    .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+                    .log("body: ${body}")
+                    .log("header: ${headers}")
+                    .to("http://localhost:8080/hello");
+
+                from("restlet:http://localhost:8080/hello?restletMethod=POST")
+                    .log("body: ${body}")
+                    .log("header: ${headers}")
+                    .transform().simple("Hello, ${body}!");
+            }
+        };
+    }
+
+    @Test
+    public void hello() {
+        String result = template.requestBody(
+            "direct:hello", "Test", String.class);
+        assertThat(result, is("Hello, Test!"));
+    }
+
+}
