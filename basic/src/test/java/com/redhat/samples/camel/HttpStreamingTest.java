87e0139c9f024d28617c1138257e19e3c227f94c
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2019-07-17 14:25:19 +0900

    add HTTP streaming sample

---
+++ basic/src/test/java/com/redhat/samples/camel/HttpStreamingTest.java
@@ -0,0 +1,112 @@
+package com.redhat.samples.camel;
+
+import com.redhat.samples.camel.helpers.StreamHelper;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * Test it with <code>-Xmx64m</code>.
+ */
+public class HttpStreamingTest extends CamelTestSupport {
+
+    private final StreamHelper stream = new StreamHelper(1000 * 100); // 100 MB
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                // @formatter:off
+                from("direct:http4-to-jetty").routeId("http4-to-jetty")
+                    .process(stream::produce)
+                    .to("http4://localhost:9001?disableStreamCache=true")
+                    .process(stream::consume)
+                    .log("length = ${body}");
+                from("jetty:http://localhost:9001?disableStreamCache=true").routeId("jetty-to-http4")
+                    .process(stream::consume)
+                    .log("length = ${body}")
+                    .to("mock:check-length")
+                    .process(stream::produce);
+
+                from("direct:http4-to-undertow").routeId("http4-to-undertow")
+                    .process(stream::produce)
+                    .to("http4://localhost:9002?disableStreamCache=true")
+                    .process(stream::consume)
+                    .log("length = ${body}");
+                from("undertow:http://localhost:9002?useStreaming=true").routeId("undertow-to-http4")
+                    .process(stream::consume)
+                    .log("length = ${body}")
+                    .to("mock:check-length")
+                    .process(stream::produce);
+
+                from("direct:http4-to-rest-undertow").routeId("http4-to-rest-undertow")
+                    .process(stream::produce)
+                    .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                    .to("http4://localhost:9003/rest/undertow/post?disableStreamCache=true")
+                    .process(stream::consume)
+                    .log("length = ${body}");
+                restConfiguration()
+                    .component("undertow")
+                    .contextPath("rest")
+                    .host("localhost")
+                    .port(9003)
+                    .endpointProperty("useStreaming", "true");
+                rest("/undertow")
+                    .post("/post").produces("text/plain")
+                    .to("direct:rest-undertow-to-http4");
+                from("direct:rest-undertow-to-http4").routeId("rest-undertow-to-http4")
+                    .process(stream::consume)
+                    .log("length = ${body}")
+                    .to("mock:check-length")
+                    .process(stream::produce);
+
+                from("direct:undertow-to-jetty").routeId("undertow-to-jetty")
+                    .process(stream::produce)
+                    .to("undertow:http://localhost:9004?useStreaming=true")
+                    .process(stream::consume)
+                    .log("length = ${body}");
+                from("jetty:http://localhost:9004?disableStreamCache=true").routeId("jetty-to-undertow")
+                    .process(stream::consume)
+                    .log("length = ${body}")
+                    .to("mock:check-length")
+                    .process(stream::produce);
+                // @formatter:on
+            }
+        };
+    }
+
+    private void doTest(String endpointUri) throws InterruptedException {
+        MockEndpoint mock = getMockEndpoint("mock:check-length");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived(stream.length());
+
+        long length = template.requestBody(endpointUri, null, Long.class);
+
+        mock.assertIsSatisfied();
+        assertEquals(stream.length(), length);
+    }
+
+    @Test
+    public void http4ToJetty() throws Exception {
+        doTest("direct:http4-to-jetty");
+    }
+
+    @Test
+    public void http4ToUndertow() throws Exception {
+        doTest("direct:http4-to-undertow");
+    }
+
+    @Test
+    public void http4ToRestUndertow() throws Exception {
+        doTest("direct:http4-to-rest-undertow");
+    }
+
+    @Test
+    public void undertowToJetty() throws Exception {
+        doTest("direct:undertow-to-jetty");
+    }
+
+}
---
+++ basic/src/test/java/com/redhat/samples/camel/helpers/StreamHelper.java
@@ -0,0 +1,74 @@
+package com.redhat.samples.camel.helpers;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import org.apache.camel.Exchange;
+
+public class StreamHelper {
+
+    /** 100 * 10 = 1 KB */
+    public static final String DEFAULT_LINE =
+        String.join("", Collections.nCopies(100, "0123456789"));
+
+    /** 100 MB */
+    public static final long DEFAULT_COUNT = 1000 * 100;
+
+    private final String line;
+    private final long count;
+
+    public StreamHelper() {
+        this(DEFAULT_LINE, DEFAULT_COUNT);
+    }
+
+    public StreamHelper(long count) {
+        this(DEFAULT_LINE, count);
+    }
+
+    public StreamHelper(String line, long count) {
+        this.line = line;
+        this.count = count;
+    }
+
+    public long length() {
+        return line.length() * count;
+    }
+
+    public void produce(Exchange exchange) throws IOException {
+        PipedOutputStream out = new PipedOutputStream();
+        exchange.getIn().setBody(new PipedInputStream(out));
+        new Thread(() -> {
+            try (OutputStreamWriter osw = new OutputStreamWriter(out);
+                 BufferedWriter writer = new BufferedWriter(osw)) {
+                LongStream.range(0, count).forEach(i -> {
+                    try {
+                        writer.write(line);
+                        writer.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void consume(Exchange exchange) throws IOException {
+        try (InputStream in = exchange.getIn().getBody(InputStream.class);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            long length = reader.lines()
+                .collect(Collectors.summingLong(String::length));
+            exchange.getIn().setBody(length);
+        }
+    }
+}
